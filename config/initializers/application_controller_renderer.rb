616c038a05ad5b4f78418a6940bebf17c5a5bbe4
dym330 <cat.of.attic@gmail.com>
2021-01-11 11:19:48 +0000

    [finish]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ddd2bb456f267e25e52faa04ee85b7513f1f97b1b7a3e4413e6cf05c61a233f42efa89e94a24219c70f170a77f67e6da8e227541c39d1401b786d123474297a4'
